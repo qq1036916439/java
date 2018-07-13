723cd7e797966fede08290f35d29bc6dfba85a4b
qq1036916439 <35517930+qq1036916439@users.noreply.github.com>
2018-07-13 20:05:14 +0800

    增加了学生成绩查询功能 用了ajax 可以通过学生姓名查找 该学生所有成绩和 通过课程 查找 所有学生的成绩，在考虑要不要加入缓存功能

---
+++ src/main/java/com/zzq/utils/SelectBean.java
@@ -0,0 +1,23 @@
+package com.zzq.utils;
+
+public class SelectBean {
+     private  Integer select;
+     private  Integer city;
+
+    public Integer getCity() {
+        return city;
+    }
+
+    public void setCity(Integer city) {
+        this.city = city;
+    }
+
+    public Integer getSelect() {
+
+        return select;
+    }
+
+    public void setSelect(Integer select) {
+        this.select = select;
+    }
+}
