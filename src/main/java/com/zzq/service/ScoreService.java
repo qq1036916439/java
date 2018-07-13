cb275e5bb116cc370ba2089eae3af158e16f5ea5
qq1036916439 <35517930+qq1036916439@users.noreply.github.com>
2018-07-13 21:50:19 +0800

    闲来无事 加入缓存 实现成绩查询的缓存功能 功能实现 使用技术redis和springcache。

--- src/main/java/com/zzq/service/ScoreService.java
+++ src/main/java/com/zzq/service/ScoreService.java
@@ -4,6 +4,8 @@ import com.zzq.mapper.ScoreMapper;
 import com.zzq.pojo.Score;
 import com.zzq.utils.SelectBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +14,11 @@ import java.util.List;
 public class ScoreService {
     @Autowired
     private ScoreMapper scoreMapper;
-
+    @CacheEvict(cacheNames = "scop",allEntries = true)
     public int addScore(Score score){
         return  scoreMapper.insert(score);
     }
+    @Cacheable(cacheNames = "scop",key = "#selectBean.select+' '+#selectBean.city")
    public List  selectScore(SelectBean selectBean){
       if(selectBean.getSelect()==1) {
        return  scoreMapper.selectSourcebyStu(selectBean.getCity());
